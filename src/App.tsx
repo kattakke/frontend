a35c899e8720164ba07e03a84f8d4391e53e6b8e
usatyo <yu0424.math8128@gmail.com>
2023-07-08 14:11:52 +0900

    change eslint, prettier setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import { useState } from "react"
-import "./App.css"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p className="text-blue-300 bg-red-600 mb-0 h-1 w-10 mt-10">
-        Hello Vite + React!
-      </p>
-      <div className="card">
+      <div className="p-20 bg-red-50 flex items-center justify-center">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
