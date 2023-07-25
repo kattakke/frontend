99bd0fa94ee0eccd07d27ef7da6068d789f995ea
shoji <gge04242.s@gmail.com>
2023-07-25 03:17:30 +0000

    change placeholder and add type

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,13 +9,13 @@ const Login = () => {
         <div className='flex-col space-y-2'>
           <p className='font-light'>メールアドレス</p>
           <div className='flex'>
-          <TextField className='flex-auto font-medium' placeholder='メールアドレス'></TextField>
+          <TextField className='flex-auto font-medium' placeholder='user@example.com' type='email'></TextField>
           </div>
         </div>
         <div className='flex-col space-y-2'>
           <p className='font-light'>パスワード</p>
           <div className='flex'>
-          <TextField className='flex-auto font-medium' placeholder='パスワード'></TextField>
+          <TextField className='flex-auto font-medium' placeholder='6文字以上で入力' type='password'></TextField>
           </div>
         </div>
         <div className='flex items-center justify-center'>
