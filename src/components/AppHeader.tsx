ca6a7c113b618f9580893f5bed624ad1127f5100
usatyo <yu0424.math8128@gmail.com>
2023-07-25 19:25:23 +0900

    add header design

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RxCross2, RxHamburgerMenu } from 'react-icons/rx'
 
 const AppHeader = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="h-16 bg-main flex items-center justify-center text-white">
-      ヘッダー
+    <div className="h-20 flex items-center justify-between text-white px-5">
+      <img src="/logo.png" alt="logo" className="h-10 w-10" />
+      {open ? (
+        <div onClick={() => setOpen(false)}>
+          <RxCross2 className="text-main h-12 w-12" />
+        </div>
+      ) : (
+        <div onClick={() => setOpen(true)}>
+          <RxHamburgerMenu className="text-main h-12 w-12" />
+        </div>
+      )}
     </div>
   )
 }
