baa6a52039e883f10490bb848ad6a8f7e93807a2
usatyo <yu0424.math8128@gmail.com>
2023-07-30 18:28:54 +0900

    fix build error

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,7 @@ import { fakerJA } from '@faker-js/faker'
 import { User } from '../types'
 
 const mockUser = (userId: string = fakerJA.string.uuid()): User => {
-  const user = {}
+  const user = { userId: userId }
   return user
 }
 
