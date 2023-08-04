85f739491aead901ca17563f6c946997e252c34e
usatyo <yu0424.math8128@gmail.com>
2023-08-04 17:37:04 +0900

    add home page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,6 +3,7 @@ import React, { ComponentPropsWithoutRef, FC, ReactNode } from 'react'
 type Props = {
   children?: ReactNode
   color?: 'main' | 'accent'
+  gradation?: boolean
   disabled?: boolean
   className?: string
 } & ComponentPropsWithoutRef<'button'>
@@ -10,28 +11,46 @@ type Props = {
 const Button: FC<Props> = ({
   children,
   color = 'main',
+  gradation = false,
   disabled = false,
   className,
   ...props
 }) => {
-  const colorStyle = {
-    main: 'bg-main',
-    accent: 'bg-accent',
+  if (gradation) {
+    const colorStyle = {
+      main: 'from-main to-main/20',
+      accent: 'from-accent to-accent/20',
+    }
+    return (
+      <button
+        className={[
+          'h-20 w-full rounded-full bg-gradient-to-br',
+          colorStyle[color],
+          className,
+        ].join(' ')}
+      >
+        {children}
+      </button>
+    )
+  } else {
+    const colorStyle = {
+      main: 'bg-main',
+      accent: 'bg-accent',
+    }
+    return (
+      <button
+        className={[
+          'text-white rounded-lg h-[36px] w-[140px]',
+          colorStyle[color],
+          className,
+        ].join(' ')}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </button>
+    )
   }
-
-  return (
-    <button
-      className={[
-        'text-white rounded-lg h-[36px] w-[140px]',
-        colorStyle[color],
-        className,
-      ].join(' ')}
-      disabled={disabled}
-      {...props}
-    >
-      {children}
-    </button>
-  )
 }
 
 export default Button
