68ed6718536dea0a051c14b2fc682f2b3f5d88c3
shoji <gge04242.s@gmail.com>
2023-08-05 02:34:54 +0000

    add signup page

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Button from '../components/Button'
+import TextField from '../components/TextField'
+import { useAuth } from '../hooks/useAuth'
+
+const Signup = () => {
+  const { login } = useAuth()
+
+  return (
+    <div className="py-8">
+      <div className="bg-white rounded-3xl px-6 py-8 space-y-6 flex-col items-center justify-center shadow-md">
+        <h1 className='text-center text-lg'>会員登録</h1>
+        <div className="flex-col space-y-2">
+          <p className="font-light">名前</p>
+          <div className="flex">
+            <TextField
+              className="flex-auto font-medium"
+              placeholder="田中太郎"
+              type="text"
+              name="name"
+            ></TextField>
+          </div>
+        </div>
+        <div className="flex-col space-y-2">
+          <p className="font-light">メールアドレス</p>
+          <div className="flex">
+            <TextField
+              className="flex-auto font-medium"
+              placeholder="user@example.com"
+              type="email"
+            ></TextField>
+          </div>
+        </div>
+        <div className="flex-col space-y-2">
+          <p className="font-light">パスワード</p>
+          <div className="flex">
+            <TextField
+              className="flex-auto font-medium"
+              placeholder="6文字以上で入力"
+              type="password"
+            ></TextField>
+          </div>
+        </div>
+        <div className="flex-col space-y-2 pb-6">
+          <p className="font-light">パスワード再入力</p>
+          <div className="flex">
+            <TextField
+              className="flex-auto font-medium"
+              placeholder="6文字以上で入力"
+              type="password"
+            ></TextField>
+          </div>
+        </div>
+        <Button className="w-full">登録</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
