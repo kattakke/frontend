6a1d967ad16a4cc7ecc31eb09a4b396d61d7f1d1
shoji <gge04242.s@gmail.com>
2023-08-05 02:58:19 +0000

    change path color

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -41,7 +41,9 @@ const Login: FC = () => {
         <div className="flex flex-col  items-center justify-center">
           <Button className="mb-3">ログイン</Button>
           <Link to={'/signup'}>
-            <p className="border-b">登録がお済みでない方はこちらから</p>
+            <p className="border-b text-sm text-main">
+              登録がお済みでない方はこちらから
+            </p>
           </Link>
         </div>
       </div>
