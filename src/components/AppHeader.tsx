8c675339a307bf7708f5fe1b42b7e0e58415d6b6
subaru <main@su8ru.dev>
2023-08-11 21:40:55 +0900

    feat: hide menu while not authorized

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -12,7 +12,7 @@ import { useAuth } from '../hooks/useAuth'
 
 const AppHeader: FC = () => {
   const [open, setOpen] = useState(false)
-  const { logout } = useAuth()
+  const { isAuth, logout } = useAuth()
   const navigate = useNavigate()
 
   const submitLogout = (): void => {
@@ -30,87 +30,91 @@ const AppHeader: FC = () => {
       <Link to="/home" className="p-2">
         <img src="/logo.png" alt="logo" className="h-10 w-10" />
       </Link>
-      {open ? (
-        <div
-          onClick={() => {
-            setOpen(false)
-          }}
-        >
-          <RxCross2 className="relative z-header-button h-12 w-12 text-white" />
-        </div>
-      ) : (
-        <div
-          onClick={() => {
-            setOpen(true)
-          }}
-        >
-          <RxHamburgerMenu className="relative z-header-button h-12 w-12 text-main" />
-        </div>
-      )}
-      <div
-        className={[
-          !open && '-translate-y-full',
-          'absolute top-0 left-0 w-full z-header bg-black/80 p-8 transition-all duration-500',
-        ].join(' ')}
-      >
-        <p className="text-xl">かったっけ</p>
-        <div className="mt-8 flex flex-col space-y-6">
-          <Link
-            to="/home"
-            className="flex w-fit items-center space-x-2"
-            onClick={() => {
-              setOpen(false)
-            }}
-          >
-            <HiOutlineHome className="h-6 w-6" />
-            <p className="text-lg">ホーム</p>
-          </Link>
-          <Link
-            to="/search"
-            className="flex w-fit items-center space-x-2"
-            onClick={() => {
-              setOpen(false)
-            }}
-          >
-            <HiOutlineSearch className="h-6 w-6" />
-            <p className="text-lg">本を検索</p>
-          </Link>
-          <Link
-            to="/register"
-            className="flex w-fit items-center space-x-2"
-            onClick={() => {
-              setOpen(false)
-            }}
-          >
-            <HiOutlinePlus className="h-6 w-6" />
-            <p className="text-lg">本を登録</p>
-          </Link>
-          <Link
-            to="/setting"
-            className="flex w-fit items-center space-x-2"
-            onClick={() => {
-              setOpen(false)
-            }}
-          >
-            <HiOutlineCog className="h-6 w-6" />
-            <p className="text-lg">設定</p>
-          </Link>
+      {isAuth && (
+        <>
+          {open ? (
+            <div
+              onClick={() => {
+                setOpen(false)
+              }}
+            >
+              <RxCross2 className="relative z-header-button h-12 w-12 text-white" />
+            </div>
+          ) : (
+            <div
+              onClick={() => {
+                setOpen(true)
+              }}
+            >
+              <RxHamburgerMenu className="relative z-header-button h-12 w-12 text-main" />
+            </div>
+          )}
           <div
-            className="flex w-fit items-center space-x-2 text-logout"
-            onClick={submitLogout}
+            className={[
+              !open && '-translate-y-full',
+              'absolute top-0 left-0 w-full z-header bg-black/80 p-8 transition-all duration-500',
+            ].join(' ')}
           >
-            <HiOutlineLogout className="h-6 w-6" />
-            <p className="text-lg">ログアウト</p>
+            <p className="text-xl">かったっけ</p>
+            <div className="mt-8 flex flex-col space-y-6">
+              <Link
+                to="/home"
+                className="flex w-fit items-center space-x-2"
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                <HiOutlineHome className="h-6 w-6" />
+                <p className="text-lg">ホーム</p>
+              </Link>
+              <Link
+                to="/search"
+                className="flex w-fit items-center space-x-2"
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                <HiOutlineSearch className="h-6 w-6" />
+                <p className="text-lg">本を検索</p>
+              </Link>
+              <Link
+                to="/register"
+                className="flex w-fit items-center space-x-2"
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                <HiOutlinePlus className="h-6 w-6" />
+                <p className="text-lg">本を登録</p>
+              </Link>
+              <Link
+                to="/setting"
+                className="flex w-fit items-center space-x-2"
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                <HiOutlineCog className="h-6 w-6" />
+                <p className="text-lg">設定</p>
+              </Link>
+              <div
+                className="flex w-fit items-center space-x-2 text-logout"
+                onClick={submitLogout}
+              >
+                <HiOutlineLogout className="h-6 w-6" />
+                <p className="text-lg">ログアウト</p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {open && (
-        <div
-          className="absolute left-0 top-0 z-overlay h-full w-full"
-          onClick={() => {
-            setOpen(false)
-          }}
-        />
+          {open && (
+            <div
+              className="absolute left-0 top-0 z-overlay h-full w-full"
+              onClick={() => {
+                setOpen(false)
+              }}
+            />
+          )}
+        </>
       )}
     </div>
   )
