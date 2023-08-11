850f345d173a31bf5b580698972afa69d5566e7d
usatyo <yu0424.math8128@gmail.com>
2023-08-11 21:09:27 +0900

    add signup function

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,13 +1,39 @@
-import { type FC } from 'react'
+import { useState, type FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Alert from '~/components/Alert'
+import { useAuth } from '~/hooks/useAuth'
 import Button from '../components/Button'
 import TextField from '../components/TextField'
 
 const Signup: FC = () => {
+  const [email, setEmail] = useState('')
+  const [firstPassword, setFirstPassword] = useState('')
+  const [secondPassword, setSecondPassword] = useState('')
+  const [alertOpen, setAlertOpen] = useState(false)
+  const [alertMessage, setAlertMessage] = useState('')
+  const { signup } = useAuth()
+  const navigate = useNavigate()
+
+  const onSignup = (): void => {
+    if (firstPassword !== secondPassword) {
+      setAlertMessage('再入力されたパスワードが異なります')
+      setAlertOpen(true)
+      return
+    }
+    signup(email, firstPassword)
+      .then(() => {
+        navigate('/home')
+      })
+      .catch((e) => {
+        throw e
+      })
+  }
+
   return (
     <div className="py-8">
       <div className="flex-col items-center justify-center space-y-6 rounded-3xl bg-white px-6 py-8 shadow-md">
-        <h1 className="text-center text-xl">会員登録</h1>
-        <div className="flex-col space-y-2">
+        <h1 className="text-center text-xl">新規登録</h1>
+        {/* <div className="flex-col space-y-2">
           <p className="font-light">名前</p>
           <div className="flex">
             <TextField
@@ -17,7 +43,7 @@ const Signup: FC = () => {
               name="name"
             ></TextField>
           </div>
-        </div>
+        </div> */}
         <div className="flex-col space-y-2">
           <p className="font-light">メールアドレス</p>
           <div className="flex">
@@ -25,6 +51,10 @@ const Signup: FC = () => {
               className="flex-auto font-medium"
               placeholder="user@example.com"
               type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
             ></TextField>
           </div>
         </div>
@@ -35,6 +65,10 @@ const Signup: FC = () => {
               className="flex-auto font-medium"
               placeholder="6文字以上で入力"
               type="password"
+              value={firstPassword}
+              onChange={(e) => {
+                setFirstPassword(e.target.value)
+              }}
             ></TextField>
           </div>
         </div>
@@ -45,12 +79,22 @@ const Signup: FC = () => {
               className="flex-auto font-medium"
               placeholder="6文字以上で入力"
               type="password"
+              value={secondPassword}
+              onChange={(e) => {
+                setSecondPassword(e.target.value)
+              }}
             ></TextField>
           </div>
         </div>
         <div className="flex items-center justify-center pb-5">
-          <Button>登録</Button>
+          <Button onClick={onSignup}>登録</Button>
         </div>
+        <Alert
+          open={alertOpen}
+          message={alertMessage}
+          variant="error"
+          onOpenChange={setAlertOpen}
+        />
       </div>
     </div>
   )
